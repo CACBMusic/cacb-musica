cbb2a303e59a8c9c8d28ccab239c8d07c3a4e606
Carlos Castillo <cacbmusica@gmail.com>
2026-05-09 14:34:42 -0400

    Add ClerkProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const geistSans = Geist({
@@ -32,7 +34,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable}`}
     >
       <body className="min-h-screen bg-white text-black transition-colors dark:bg-[#050505] dark:text-white">
-        <ThemeProvider>{children}</ThemeProvider>
+        <ClerkProvider>
+  <ThemeProvider>{children}</ThemeProvider>
+</ClerkProvider>
       </body>
     </html>
   );
